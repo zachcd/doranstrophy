56884be9aec81247e1cddbb1b5f21ee39bf08be2
Connor Lewellyn <lewellync@gmail.com>
2016-05-07 14:36:55 -0400

    Stat aggregators started

--- dorantrophy/cards/views.py
+++ dorantrophy/cards/views.py
@@ -14,11 +14,16 @@ def cards(request):
 		summoner = posted.get('summ')
 		region = posted.get('region')
 		riotapi.set_region(region)
-		summ = riotapi.get_summoner_by_name(summoner)
+		try:
+			summ = riotapi.get_summoner_by_name(summoner)
+			pass
+		except Exception, e:
+			raise e
 		masteries = riotapi.get_champion_masteries(summ)
 		trophyChampions = {}
 		count = 0
 		#this sets up so that champions with lvl 4 or higher mastery but if less than 4 are found add more from lower lvls
+
 		for champion, mastery in masteries:
 			if mastery.level >= 4 :
 				trophyChampions[champion] = mastery
@@ -29,10 +34,23 @@ def cards(request):
 				trophyChampions[champion] = mastery
 		for c in trophyChampions:
 			topKDA = 0
-			Kills = 0
-			Deaths = 0
-			Assists = 0
-			Wins = 0
+			kills = 0
+			deaths = 0
+			assists = 0
+			wins = 0
+			losses = 0
 			matches = riotapi.get_match_list(summ,champion_ids = c, seasons=“SEASON2016”)
 			for m in matches:
 				data = riotapi.get_match(m)
+				for player in data.participants:
+					if player.summoner_name == summoner: #Build stats from this players participant object
+						playerStats = player.stats
+						topKDA = stats.kda if stats.kda > topKDA else topKDA
+						kills += stats.kills
+						deaths += stats.deaths
+						assists += stats.assists
+						if win:
+							wins = win + 1
+						else:
+							losses = losses + 1
+
