f5851e732498860060e90a0053c798d609051e13
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2016-05-07 13:15:53 -0400

    added some basic stuff to prepare for dev

--- dorantrophy/cards/views.py
+++ dorantrophy/cards/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from cassiopeia import riotapi
 # Create your views here.
+
+
+def index(request):
+	#this will be a simple page to take summoner name input
+
+def cards(request):
+	if request.GET:
+		riotapi.set_api_key("3ca655bb-959d-4ed3-ab2c-287da159aa59")
+		getted = request.GET
+		summoner = posted.get('summ')
+		region = posted.get('region')
+		riotapi.set_region(region)
+		riotapi.get_summoner_by_name()
