068f10f2e106bee87b0d73de96e1e02c45bc55b9
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2016-05-10 02:22:14 -0400

    oops

--- dorantrophy/cards/views.py
+++ dorantrophy/cards/views.py
@@ -9,7 +9,6 @@ def index(request):
 
 def cards(request):
 	if request.GET:
-		riotapi.set_api_key("3ca655bb-959d-4ed3-ab2c-287da159aa59")
 		getted = request.GET
 		summoner = posted.get('summ')
 		region = posted.get('region')
